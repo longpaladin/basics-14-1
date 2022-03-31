9752d5f1186e66c5278186d8afba47572ff4cfa8
longpaladin <30802551+longpaladin@users.noreply.github.com>
2022-03-31 14:26:38 +0800

    day3 pre

--- day03-program-state/pre-class/script.js
+++ day03-program-state/pre-class/script.js
@@ -1,53 +1,96 @@
 var introScopeMain = function (input) {
   // Attempt the Follow Along exercise from the Intro to Scope module below with introScopeMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
+};
+
+var rollDice = function () {
+  // produces a decimal between 0 and 6
+  var randomDecimal = Math.random() * 6;
+
+  // take off the decimal
+  var randomInteger = Math.floor(randomDecimal);
+
+  // it's a number from 0 - 5 ... add 1
+  var diceNumber = randomInteger + 1;
+
+  return diceNumber;
 };
 
 var appSetupMain = function (input) {
   // Attempt the Base: App Setup exercise from the Program Lifecycle and State module below with appSetupMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+
+  var myOutputValue = "you lose";
+
+  if (randomDiceNumber == input) {
+    myOutputValue = "you win";
+  }
+
   return myOutputValue;
 };
+
+var previousRoll = 1;
 
 var lastRollMain = function (input) {
   // Attempt the Base: Last Roll exercise from the Program Lifecycle and State module below with lastRollMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+
+  var myOutputValue = `your last roll was ${previousRoll}. you just rolled a ${randomDiceNumber}. you guessed ${input}. you lose`;
+
+  if (randomDiceNumber == input) {
+    myOutputValue = `your last roll was ${previousRoll}. you just rolled a ${randomDiceNumber}. you guessed ${input}. you win`;
+  }
+  previousRoll = randomDiceNumber;
   return myOutputValue;
 };
 
+var numberOfPlays = 0;
+var wins = 0;
+var percentageWin;
+
 var winLossMain = function (input) {
   // Attempt the Base: Win / Loss exercise from the Program Lifecycle and State module below with winLossMain as the main function.
-  var myOutputValue = 'hello world';
+  var randomDiceNumber = rollDice();
+  numberOfPlays += 1;
+  percentageWin = wins / numberOfPlays;
+  var myOutputValue = `you win ${percentageWin} of the time. you just rolled a ${randomDiceNumber}. you guessed ${input}. you lose`;
+
+  if (randomDiceNumber == input) {
+    wins += 1;
+    percentageWin = wins / numberOfPlays;
+    myOutputValue = `you win ${percentageWin} of the time. you just rolled a ${randomDiceNumber}. you guessed ${input}. you win`;
+  }
+  previousRoll = randomDiceNumber;
   return myOutputValue;
 };
 
 var mostRolledMain = function (input) {
   // Attempt the More Comfortable: Most Rolled exercise from the Program Lifecycle and State module below with mostRolledMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var guessingMain = function (input) {
   // Attempt the More Comfortable: Guessing exercise from the Program Lifecycle and State module below with guessingMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var advancedGuessingMain = function (input) {
   // Attempt the More Comfortable: Advanced Guessing exercise from the Program Lifecycle and State module below with advancedGuessingMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var followAlongMain = function (input) {
   // Attempt the Follow Along exercise from the Program State for Game Modes module below with followAlongMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
 
 var redModeMain = function (input) {
   // Attempt the Red Mode exercise from the Program State for Game Modes module below with redModeMain as the main function.
-  var myOutputValue = 'hello world';
+  var myOutputValue = "hello world";
   return myOutputValue;
 };
